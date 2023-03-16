a2940d6e4a500707946f976404c99b6a89d07e74
YujithIsura <ujithisura@gmail.com>
2023-03-16 10:26:44 +0530

    profile information widget WIP

--- campus/frontend/lib/main.dart
+++ campus/frontend/lib/main.dart
@@ -17,8 +17,8 @@ import 'package:gallery/pages/splash.dart';
 import 'package:gallery/routes.dart';
 import 'package:gallery/themes/gallery_theme_data.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:sizer/sizer.dart';
 import 'package:url_strategy/url_strategy.dart';
-// export 'package:gallery/data/demos.dart' show pumpDeferredLibraries;
 
 void main() async {
   // Use package:url_strategy until this pull request is released:
@@ -66,54 +66,58 @@ class _GalleryAppState extends State<GalleryApp> {
 
   @override
   Widget build(BuildContext context) {
-    return ModelBinding(
-      initialModel: GalleryOptions(
-        themeMode: ThemeMode.system,
-        textScaleFactor: systemTextScaleFactorOption,
-        customTextDirection: CustomTextDirection.localeBased,
-        locale: null,
-        timeDilation: timeDilation,
-        platform: defaultTargetPlatform,
-        isTestMode: isTestMode,
-      ),
-      child: Builder(
-        builder: (context) {
-          final options = GalleryOptions.of(context);
-          return MaterialApp(
-              restorationScopeId: 'rootGallery',
-              title: 'Avinya Academy',
-              debugShowCheckedModeBanner: false,
-              navigatorObservers: [routeObserver],
-              themeMode: options.themeMode,
-              theme: GalleryThemeData.lightThemeData.copyWith(
-                platform: options.platform,
-              ),
-              darkTheme: GalleryThemeData.darkThemeData.copyWith(
-                platform: options.platform,
-              ),
-              localizationsDelegates: const [
-                ...GalleryLocalizations.localizationsDelegates,
-                LocaleNamesLocalizationsDelegate()
-              ],
-              initialRoute: loginRoute,
-              supportedLocales: GalleryLocalizations.supportedLocales,
-              locale: options.locale,
-              localeListResolutionCallback: (locales, supportedLocales) {
-                deviceLocale = locales?.first;
-                return basicLocaleListResolution(locales, supportedLocales);
-              },
-              onGenerateRoute: (settings) {
-                return RouteConfiguration.onGenerateRoute(settings);
-              },
-              onUnknownRoute: (RouteSettings settings) {
-                return MaterialPageRoute<void>(
-                  settings: settings,
-                  builder: (BuildContext context) =>
-                      Scaffold(body: Center(child: Text('Not Found'))),
-                );
-              });
-        },
-      ),
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return ModelBinding(
+          initialModel: GalleryOptions(
+            themeMode: ThemeMode.system,
+            textScaleFactor: systemTextScaleFactorOption,
+            customTextDirection: CustomTextDirection.localeBased,
+            locale: null,
+            timeDilation: timeDilation,
+            platform: defaultTargetPlatform,
+            isTestMode: isTestMode,
+          ),
+          child: Builder(
+            builder: (context) {
+              final options = GalleryOptions.of(context);
+              return MaterialApp(
+                  restorationScopeId: 'rootGallery',
+                  title: 'Avinya Academy',
+                  debugShowCheckedModeBanner: false,
+                  navigatorObservers: [routeObserver],
+                  themeMode: options.themeMode,
+                  theme: GalleryThemeData.lightThemeData.copyWith(
+                    platform: options.platform,
+                  ),
+                  darkTheme: GalleryThemeData.darkThemeData.copyWith(
+                    platform: options.platform,
+                  ),
+                  localizationsDelegates: const [
+                    ...GalleryLocalizations.localizationsDelegates,
+                    LocaleNamesLocalizationsDelegate()
+                  ],
+                  initialRoute: loginRoute,
+                  supportedLocales: GalleryLocalizations.supportedLocales,
+                  locale: options.locale,
+                  localeListResolutionCallback: (locales, supportedLocales) {
+                    deviceLocale = locales?.first;
+                    return basicLocaleListResolution(locales, supportedLocales);
+                  },
+                  onGenerateRoute: (settings) {
+                    return RouteConfiguration.onGenerateRoute(settings);
+                  },
+                  onUnknownRoute: (RouteSettings settings) {
+                    return MaterialPageRoute<void>(
+                      settings: settings,
+                      builder: (BuildContext context) =>
+                          Scaffold(body: Center(child: Text('Not Found'))),
+                    );
+                  });
+            },
+          ),
+        );
+      },
     );
   }
 }
--- campus/frontend/lib/pages/profile_info_view.dart
+++ campus/frontend/lib/pages/profile_info_view.dart
@@ -1,117 +1,175 @@
 import 'package:gallery/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:gallery/data/campus_apps_portal.dart';
+import 'package:gallery/data/person.dart';
 import 'package:sizer/sizer.dart';
 
-class MyProfileScreen extends StatelessWidget {
+class MyProfileScreen extends StatefulWidget {
   const MyProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  _MyProfileScreenState createState() => _MyProfileScreenState();
+}
+
+class _MyProfileScreenState extends State<MyProfileScreen> {
+  late Person userPerson;
+  // String? preferredName;
+  // String? fullName;
+  // String? notes;
+
+  @override
+  void initState() {
+    super.initState();
+    getUserPerson();
+  }
+
+  void getUserPerson() {
+    // Retrieve user data from local instance
+    Person user = campusAppsPortalInstance.getUserPerson();
+    setState(() {
+      userPerson = user;
+    });
+  }
+
+  // String? preferredName = userPerson.preferred_name;
+
+  //  MyProfileScreen({Key? key}) : super(key: key);
   static String routeName = 'MyProfileScreen';
+
+  //log user details
+  // print('userPerson: ${userPerson.id}');
+  // print('userPerson: ${userPerson.preferred_name}');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       //app bar theme for tablet
-      appBar: AppBar(
-        title: Text('My Profile'),
-        actions: [
-          InkWell(
-            onTap: () {
-              //send report to school management, in case if you want some changes to your profile
-            },
-            child: Container(
-              padding: EdgeInsets.only(right: kDefaultPadding / 2),
-              child: Row(
+      // appBar: AppBar(
+      //   title: Text('My Profile'),
+      //   actions: [
+      //     InkWell(
+      //       onTap: () {
+      //         //send report to school management, in case if you want some changes to your profile
+      //       },
+      //       child: Container(
+      //         padding: EdgeInsets.only(right: kDefaultPadding / 2),
+      //         child: Row(
+      //           children: [
+      //             Icon(Icons.report_gmailerrorred_outlined),
+      //             kHalfWidthSizedBox,
+      //             Text(
+      //               'Report',
+      //               style: Theme.of(context).textTheme.subtitle2,
+      //             ),
+      //           ],
+      //         ),
+      //       ),
+      //     ),
+      //   ],
+      // ),
+      body: SingleChildScrollView(
+        child: Container(
+          color: kOtherColor,
+          child: Column(
+            children: [
+              Container(
+                width: 100.w,
+                height: SizerUtil.deviceType == DeviceType.tablet ? 19.h : 15.h,
+                decoration: BoxDecoration(
+                  color: kPrimaryColor,
+                  borderRadius: kBottomBorderRadius,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      radius: SizerUtil.deviceType == DeviceType.tablet
+                          ? 12.w
+                          : 13.w,
+                      backgroundColor: kSecondaryColor,
+                      backgroundImage:
+                          AssetImage('assets/images/student_profile.jpeg'),
+                    ),
+                    kWidthSizedBox,
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          userPerson.full_name!,
+                          style: Theme.of(context)
+                              .textTheme
+                              .subtitle1!
+                              .copyWith(
+                                color: kTextBlackColor,
+                                fontSize:
+                                    SizerUtil.deviceType == DeviceType.tablet
+                                        ? 4.sp
+                                        : 5.sp,
+                              ),
+                          // style: Theme.of(context).textTheme.subtitle1,
+                        ),
+                        Text(
+                          'Class ${userPerson.avinya_type_id} | Roll no: 12',
+                          style: Theme.of(context)
+                              .textTheme
+                              .subtitle1!
+                              .copyWith(
+                                color: kTextBlackColor,
+                                fontSize:
+                                    SizerUtil.deviceType == DeviceType.tablet
+                                        ? 4.sp
+                                        : 5.sp,
+                              ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              sizedBox,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Icon(Icons.report_gmailerrorred_outlined),
-                  kHalfWidthSizedBox,
-                  Text(
-                    'Report',
-                    style: Theme.of(context).textTheme.subtitle2,
-                  ),
+                  ProfileDetailRow(
+                      title: 'Registration Number', value: '2020-ASDF-2021'),
+                  ProfileDetailRow(title: 'Academic Year', value: '2020-2021'),
                 ],
               ),
-            ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ProfileDetailRow(title: 'Admission Class', value: 'X-II'),
+                  ProfileDetailRow(title: 'Admission Number', value: '000126'),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ProfileDetailRow(
+                      title: 'Date of Admission', value: '1 Aug, 2020'),
+                  ProfileDetailRow(title: 'Date of Birth', value: '3 May 1998'),
+                ],
+              ),
+              sizedBox,
+              ProfileDetailColumn(
+                title: 'Email',
+                value: 'aisha12@gmail.com',
+              ),
+              ProfileDetailColumn(
+                title: 'Father Name',
+                value: 'John Mirza',
+              ),
+              ProfileDetailColumn(
+                title: 'Mother Name',
+                value: 'Angelica Mirza',
+              ),
+              ProfileDetailColumn(
+                title: 'Phone Number',
+                value: '+923066666666',
+              ),
+            ],
           ),
-        ],
-      ),
-      body: Container(
-        color: kOtherColor,
-        child: Column(
-          children: [
-            Container(
-              width: 100.w,
-              height: SizerUtil.deviceType == DeviceType.tablet ? 19.h : 15.h,
-              decoration: BoxDecoration(
-                color: kPrimaryColor,
-                borderRadius: kBottomBorderRadius,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CircleAvatar(
-                    radius:
-                        SizerUtil.deviceType == DeviceType.tablet ? 12.w : 13.w,
-                    backgroundColor: kSecondaryColor,
-                    backgroundImage:
-                        AssetImage('assets/images/student_profile.jpeg'),
-                  ),
-                  kWidthSizedBox,
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Aisha Mirza',
-                        style: Theme.of(context).textTheme.subtitle1,
-                      ),
-                      Text('Class X-II A | Roll no: 12',
-                          style: Theme.of(context).textTheme.subtitle2),
-                    ],
-                  )
-                ],
-              ),
-            ),
-            sizedBox,
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                ProfileDetailRow(
-                    title: 'Registration Number', value: '2020-ASDF-2021'),
-                ProfileDetailRow(title: 'Academic Year', value: '2020-2021'),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                ProfileDetailRow(title: 'Admission Class', value: 'X-II'),
-                ProfileDetailRow(title: 'Admission Number', value: '000126'),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                ProfileDetailRow(
-                    title: 'Date of Admission', value: '1 Aug, 2020'),
-                ProfileDetailRow(title: 'Date of Birth', value: '3 May 1998'),
-              ],
-            ),
-            sizedBox,
-            ProfileDetailColumn(
-              title: 'Email',
-              value: 'aisha12@gmail.com',
-            ),
-            ProfileDetailColumn(
-              title: 'Father Name',
-              value: 'John Mirza',
-            ),
-            ProfileDetailColumn(
-              title: 'Mother Name',
-              value: 'Angelica Mirza',
-            ),
-            ProfileDetailColumn(
-              title: 'Phone Number',
-              value: '+923066666666',
-            ),
-          ],
         ),
       ),
     );
@@ -139,12 +197,20 @@ class ProfileDetailRow extends StatelessWidget {
                 style: Theme.of(context).textTheme.subtitle1!.copyWith(
                       color: kTextBlackColor,
                       fontSize: SizerUtil.deviceType == DeviceType.tablet
-                          ? 7.sp
-                          : 9.sp,
+                          ? 5.sp
+                          : 6.sp,
                     ),
               ),
               kHalfSizedBox,
-              Text(value, style: Theme.of(context).textTheme.caption),
+              Text(
+                value,
+                style: Theme.of(context).textTheme.subtitle1!.copyWith(
+                      color: kTextBlackColor,
+                      fontSize: SizerUtil.deviceType == DeviceType.tablet
+                          ? 4.sp
+                          : 5.sp,
+                    ),
+              ),
               kHalfSizedBox,
               SizedBox(
                 width: 35.w,
@@ -156,7 +222,7 @@ class ProfileDetailRow extends StatelessWidget {
           ),
           Icon(
             Icons.lock_outline,
-            size: 10.sp,
+            size: 6.sp,
           ),
         ],
       ),
@@ -185,12 +251,20 @@ class ProfileDetailColumn extends StatelessWidget {
                 style: Theme.of(context).textTheme.subtitle1!.copyWith(
                       color: kTextBlackColor,
                       fontSize: SizerUtil.deviceType == DeviceType.tablet
-                          ? 7.sp
-                          : 11.sp,
+                          ? 5.sp
+                          : 6.sp,
                     ),
               ),
               kHalfSizedBox,
-              Text(value, style: Theme.of(context).textTheme.caption),
+              Text(
+                value,
+                style: Theme.of(context).textTheme.subtitle1!.copyWith(
+                      color: kTextBlackColor,
+                      fontSize: SizerUtil.deviceType == DeviceType.tablet
+                          ? 4.sp
+                          : 5.sp,
+                    ),
+              ),
               kHalfSizedBox,
               SizedBox(
                 width: 92.w,
@@ -202,7 +276,7 @@ class ProfileDetailColumn extends StatelessWidget {
           ),
           Icon(
             Icons.lock_outline,
-            size: 10.sp,
+            size: 6.sp,
           ),
         ],
       ),
