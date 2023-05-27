e539779ea2968d726f29ae73725d2588f2c9580d
YujithIsura <ujithisura@gmail.com>
2023-05-27 23:17:56 +0530

    lint

--- campus/frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ campus/frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,8 @@ import Foundation
 
 import flutter_appauth
 import package_info_plus_macos
-import path_provider_foundation
-import shared_preferences_foundation
+import path_provider_macos
+import shared_preferences_macos
 import url_launcher_macos
 import window_size
 
